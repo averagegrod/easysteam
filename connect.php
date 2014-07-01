b2e2d7d6b5affc6b8691d057eb0cf3e236d356db
averagegrod <eloso79@gmail.com>
2014-07-01 15:42:05 -0700

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+-<?php
+ -	$apikey = ''; // put your API key here 
+ -		switch ($_POST['request']){
+ -		case "getPlayerSummaries":
+ -		$response = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . $apikey . '&steamids=' . $_POST["steamID"]);
+ -		break;
+ -		case "getOwnedGames" :
+ -		$response = file_get_contents('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' . $apikey . '&steamid=' . $_POST["steamID"]  . '&format=json&include_appinfo=1');
+ -		break;
+ -	}
+ -	echo $response;
+ -	?>
